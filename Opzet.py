7e2a2bed9c6cf24fd954014019fce06df4fe0dcc
sam <sameijpe@hotmail.com>
2023-01-13 16:51:48 +0100

    comments geplaatst

--- Opzet.py
+++ Opzet.py
@@ -15,64 +15,68 @@ class Experiment():
         self.make_cables()
         self.draw_plot()
 
-
     def add_batteries(self, batteries_df):
         """
         This function creates a list with all battery instances as objects and
         adds them to a combined list of houses and batteries.
         """
+        # create list in which to store the batteries
         self.battery_list = []
 
+        # create battery instances and saves them in a battery list and in
+        # houses and batteries list
         for index, row in batteries_df.iterrows():
             battery = Batteries(row[0], row[1], row[2])
             self.houses_and_batteries.append(battery)
             self.battery_list.append(battery)
 
-        # maak key van battery in die dict
-
     def add_houses(self, houses_df):
-        '''
+        """
         This function creates a list with all house instances as objects and
         adds them to a combined list of houses and batteries.
-        '''
+        """
+        # create list in which to store houses
         self.house_list = []
 
+        # add house instances to the batteries and houses list and in the
+        # houses list
         for index, row in houses_df.iterrows():
             house = Houses(row[0], row[1], row[2])
             self.houses_and_batteries.append(house)
             self.house_list.append(house)
 
-            # maak dictionary per house aan
-
-
     def draw_plot(self):
-        '''
+        """
         This function draws a plot of the houses and batteries in the village.
-        '''
+        """
+        # create lists in which to store the information for the plot
         pos_x_list = []
         pos_y_list = []
         color_list = []
         shape_list = []
 
+        # find x and y coordinates and the color of the thing to be plotted
         for thing in self.houses_and_batteries:
             pos_x_list.append(thing.x)
             pos_y_list.append(thing.y)
             color_list.append(thing.color)
 
+        # plot all houses and batteries
         plt.scatter(pos_x_list, pos_y_list, color=color_list, marker='s', s=40)
-
         plt.show()
 
     def assign_house_random(self):
         """
         This function assigns the houses to a randomly selected battery.
         """
-        # randomly assign a battery for each house
+        # randomly assign a battery to each house
         for house in self.house_list:
             assigned_battery = random.choice(self.battery_list)
 
+            # create copy of the battery list for later
             new_battery_list = copy.copy(self.battery_list)
 
+            # if the house doesn't fit the battery anymore, choose another battery
             while house.maxoutput > assigned_battery.capacity:
 
                 # make copy of battery_list and remove the full battery
@@ -86,10 +90,10 @@ class Experiment():
                     print('list is empty')
                     break
 
+            # adjust the capacity of the battery
             assigned_battery.capacity -= house.maxoutput
-            print(assigned_battery.capacity)
-            print(count)
 
+            # save the dictionary of the house in the list of houses for that battery
             assigned_battery.dict['connected houses'].append(house.dict)
 
     def make_cables(self):
@@ -98,9 +102,6 @@ class Experiment():
         the individual steps within the grid. It then saves the individual
         coordinates in a new list.
         """
-        # every battery has a list of houses,
-        # over
-
         # loop over batteries and the houses that are connected to that battery
         for battery in self.battery_list:
             for house_dict in battery.dict['connected houses']:
@@ -115,28 +116,34 @@ class Experiment():
                 y_list = [location_house_y, location_house_y, location_battery_y]
                 plt.plot(x_list, y_list, 'k--')
 
+                # create starting point for creating the grid line
                 x_loc = location_house_x
                 y_loc = location_house_y
+
+                # compute distance to use as a constraint for choosing which way
+                # to move on the grid line
                 distance_x = location_house_x - location_battery_x
                 distance_y = location_house_y - location_battery_y
 
+                # take steps until the correct x coordinate is reached
                 while x_loc != location_battery_x:
                     if distance_x > 0:
                         x_loc -= 1
                     else:
                         x_loc += 1
 
+                    # save the individual steps in the grid list in the dictionary of the house
                     house_dict['grid'].append(f'{x_loc}, {y_loc}')
 
+                # take steps until the correct y coordinate is reached
                 while y_loc != location_battery_y:
                     if distance_y > 0:
                         y_loc -= 1
                     else:
                         y_loc += 1
 
+                    # save the individual steps in the grid list in the dictionary of the house
                     house_dict['grid'].append(f'{x_loc}, {y_loc}')
-
-                print(house_dict)
 
 class Batteries():
     def __init__(self, capacity, x, y):
@@ -159,7 +166,7 @@ class Houses():
 
 def load_df(houses_csv, batteries_csv):
     """
-    This function loads 
+    This function loads the villages and saves them as dataframes
     """
     df_houses = pd.read_csv(houses_csv)
     df_batteries = pd.read_csv(batteries_csv)
