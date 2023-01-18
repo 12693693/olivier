0bf313f061c93edad1f4d59f99e7ed926b5bf444
Frederique <frederique.mulder@gmail.com>
2023-01-18 15:24:40 +0100

    raar

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import random
 import copy
 from code.Classes.smartgrid import Smartgrid
 from code.Algorithms.randomize import Randomize
-from code.Algorithms.cable_90_degree import Cables
 #
 # dit moet eigenlijk in classmethod
 def load_df(houses_csv, batteries_csv):
@@ -58,9 +57,6 @@ if __name__ == "__main__":
     random_algo = Randomize()
     random_algo.assign_house_random(houses, batteries)
 
-    cable_90 = Cables()
-    cable_90.make_90_degrees_cables(houses, batteries)
 
     # for battery in batteries:
     #     print(battery.dict)
-    #my_smartgrid.make_90_degrees_cables(houses, batteries)
