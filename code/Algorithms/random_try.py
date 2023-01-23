1678497c5863b4839715a7ba7ee9f4ab5ee23f0e
Lisa <lisaoverbosch@gmail.com>
2023-01-23 11:57:25 +0100

    corrected indent error

--- code/Algorithms/random_try.py
+++ code/Algorithms/random_try.py
@@ -15,8 +15,8 @@ class Cables():
                 # find x and y coordinates for the battery and connected house
                 location_house_x = house_dict['house location'][0]
                 location_house_y = house_dict['house location'][1]
-                location_battery_x = battery.dict['battery location'][0]
-                location_battery_y = battery.dict['battery location'][1]
+                # location_battery_x = battery.dict['battery location'][0]
+                # location_battery_y = battery.dict['battery location'][1]
 
                 # compute distance between the battery and the assigned house
                 distance = abs((location_battery_x - location_house_x) + (location_battery_y - location_house_y))
