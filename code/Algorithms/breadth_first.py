6e70d8bb0b1bb0b4753f7e1d7952ee9e8a71be25
sam <sameijpe@hotmail.com>
2023-01-26 16:19:16 +0100

    breadth first af maar hij vult gek

--- code/Algorithms/breadth_first.py
+++ code/Algorithms/breadth_first.py
@@ -9,21 +9,19 @@ class Breadth_first():
         this function computes the distance between the battery and the starting
         location on the x and y axis
         '''
-        x_distance = x_battery - x_loc
-        y_distance = y_battery - y_loc
+        x_distance = int(x_battery) - int(x_loc)
+        y_distance = int(y_battery) - int(y_loc)
+        # print(x_distance, y_distance)
 
         return x_distance, y_distance
 
 
     def breadth(self, list_with_houses, list_with_batteries):
-        # distance y
-        steps_list = []
-        grid_list = []
-        possible_grids = []
-        # steps_dict = {'L': 0, 'R': 0, 'U': 0, 'D': 0}
 
         for battery in list_with_batteries:
+            # print('battery')
             for house_dict in battery.dict['connected houses']:
+                steps_list = []
 
                 # find x and y coordinates for the battery and connected house
                 x_loc = house_dict['house location'][0]
@@ -45,6 +43,7 @@ class Breadth_first():
                 else:
                     for i in range(x_distance):
                         steps_list.append('R')
+
                 if y_distance < 0:
                     for i in range(abs(y_distance)):
                         steps_list.append('D')
@@ -52,12 +51,15 @@ class Breadth_first():
                     for i in range(y_distance):
                         steps_list.append('U')
 
+                possible_grids = []
+
                 for i in range(10):
                     # shuffle the steps in the list randomly
                     random.shuffle(steps_list)
+                    grid_list = []
 
                     # take the random order of steps
-                    for step in steps_dict:
+                    for step in steps_list:
                         if step == 'L':
                             x_loc -= 1
                         elif step == 'R':
@@ -71,47 +73,53 @@ class Breadth_first():
                         y_list.append(y_loc)
 
                         # list containing steps for one grid
-                        grid_list.append(f'{x_loc}, {y_loc}')
+                        grid_list.append([x_loc, y_loc])
 
                     # list containing each grids list of steps
                     possible_grids.append(grid_list)
 
-            list_distances_all_grids = []
+                list_distances_all_grids = []
 
-            # loop over grids
-            for grid in possible_grids:
-                distance_to_grid_list = []
+                # loop over grids
+                for grid in possible_grids:
+                    distance_to_grid_list = []
+                    # print(len(grid))
+                    # loop over house_dicts
+                    for house_dict in battery.dict['connected houses']:
+                        x_house = house_dict['house location'][0]
+                        y_house = house_dict['house location'][1]
 
-                # loop over house_dicts
-                for house_dict in battery.dict['connected houses']:
-                    x_house = house_dict['house location'].split(',')[0]
-                    y_house = house_dict['house location'].split(',')[1]
+                        distance = 100
 
-                    distance = 100
+                        # loop over each step in the grid, and calculate distance between
+                        # the current house and the cable
+                        for step in grid:
+                            x_loc = step[0]
+                            y_loc = step[1]
+                            # print('for step in grid')
+                            # calculate current_distance
+                            current_distance = abs(x_house - x_loc) + abs(y_house - y_loc)
+                            # print('current distance')
+                            if current_distance < distance:
+                                distance = current_distance
 
-                    # loop over each step in the grid, and calculate distance between
-                    # the current house and the cable
-                    for step in grid:
-                        x_loc = step.split(',')[0]
-                        y_loc = step.split(',')[1]
+                        distance_to_grid_list.append(distance)
+                        # print('list of distances for one grid')
+                    list_distances_all_grids.append(distance_to_grid_list)
+                    # print('total list of distances all grids')
 
-                        # calculate current_distance
-                        current_distance = abs(x_house - x_loc) + abs(y_house - y_loc)
+                lowest_distance = 100
+                for index, grid_distance_list in enumerate(list_distances_all_grids):
+                    total_distance = sum(grid_distance_list)
+                    if total_distance < lowest_distance:
+                        best_grid = possible_grids[index]
+                        lowest_distance = total_distance
 
-                        if current_distance < distance:
-                            distance = current_distance
+                        house_dict['grid'] = best_grid
+                        print(house_dict['grid'])
 
-                    distance_to_grid_list.append(distance)
-                list_distances_all_grids.append(distance_to_grid_list)
-
-            lowest_distance = 100
-            for index, grid_distance_list in enumerate(list_distances_all_grids):
-                current_distance = sum(grid_distance_list)
-                if total_distance < lowest_distance:
-                    best_grid = possible_grids[index]
-                    lowest_distance = total_distance
-
-            house_dict['grid'] = best_grid
+            # print(house_dict['grid'])
+            print(battery.dict['connected houses'])
 
 
 
