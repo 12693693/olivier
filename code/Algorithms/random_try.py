26e7833b5d66cabe4bec6b2219e36bd52713bb71
sam <sameijpe@hotmail.com>
2023-01-26 10:51:52 +0100

    random try verdeeld in kleinere functies

--- code/Algorithms/random_try.py
+++ code/Algorithms/random_try.py
@@ -7,13 +7,14 @@ class Cables():
         self.y_list = []
 
     def compute_distance(self, x_battery, y_battery, x_loc, y_loc):
-        """ this function calculates the distance between location of the battery
-        and current location on grid line """
+        '''
+        This function calculates the distance between location of the battery
+        and current location on grid line
+        '''
 
         distance = abs(x_battery - x_loc) + abs(y_battery - y_loc)
 
         return distance
-
 
     def random_try(self, list_with_houses, list_with_batteries):
         '''
@@ -21,27 +22,41 @@ class Cables():
         by taking a random step, evaluating if this step is closer to the battery
         and repeating the process
         '''
-        self.steps_count = 0
+
         for battery in list_with_batteries:
             for house_dict in battery.dict['connected houses']:
 
                 # find x and y coordinates for the battery and connected house
                 x_loc = house_dict['house location'][0]
                 y_loc = house_dict['house location'][1]
-                # location_battery_x = battery.x
-                # location_battery_y = battery.y
+
+    def save_step(self, old_distance, new_distance, house_dict, x_loc, y_loc):
+        '''
+        in this function a new distance is set, and the step is saved to the grid lists
+        '''
+
+        distance = new_distance
+
+        # save the individual steps in the grid list in the dictionary of the house
+        house_dict['grid'].append(f'{x_loc}, {y_loc}')
+
+        self.x_list.append(x_loc)
+        self.y_list.append(y_loc)
+
+        return distance
 
 
     def try_steps(self, x_battery, y_battery, x_loc, y_loc, distance, house_dict):
-        ''' this function takes a starting point and a battery location, and repeatedly
+        '''
+        This function takes a starting point and a battery location, and repeatedly
         takes steps in random directions, checks if this random step improved
-        the state. If so, the step is taken, if not, the step is reset. '''
+        the state. If so, the step is taken, if not, the step is reset.
+        '''
+
         # compute distance between the battery and the assigned house
         # distance = self.compute_distance(x_battery, y_battery, x_loc, y_loc)
         while distance != 0:
             choice = random.randint(1, 4)
-
-
 
             # take a step left
             if choice == 1:
@@ -52,14 +67,7 @@ class Cables():
 
                 # if the new point is closer to the battery location, take the step
                 if new_distance < distance:
-                    distance = new_distance
-                    self.steps_count += 1
-
-                    # save the individual steps in the grid list in the dictionary of the house
-                    house_dict['grid'].append(f'{x_loc}, {y_loc}')
-
-                    self.x_list.append(x_loc)
-                    self.y_list.append(y_loc)
+                    distance = self.save_step(distance, new_distance, house_dict, x_loc, y_loc)
 
                 # if the new point is not closer to the battery location,
                 # reset the step
@@ -75,14 +83,7 @@ class Cables():
 
                 # if the new point is closer to the battery location, take the step
                 if new_distance < distance:
-                    distance = new_distance
-                    self.steps_count += 1
-
-                    # save the individual steps in the grid list in the dictionary of the house
-                    house_dict['grid'].append(f'{x_loc}, {y_loc}')
-
-                    self.x_list.append(x_loc)
-                    self.y_list.append(y_loc)
+                    distance = self.save_step(distance, new_distance, house_dict, x_loc, y_loc)
 
                 # if the new point is not closer to the battery location,
                 # reset the step
@@ -98,14 +99,7 @@ class Cables():
 
                 # if the new point is closer to the battery location, take the step
                 if new_distance < distance:
-                    distance = new_distance
-                    self.steps_count += 1
-
-                    # save the individual steps in the grid list in the dictionary of the house
-                    house_dict['grid'].append(f'{x_loc}, {y_loc}')
-
-                    self.x_list.append(x_loc)
-                    self.y_list.append(y_loc)
+                    distance = self.save_step(distance, new_distance, house_dict, x_loc, y_loc)
 
                 # if the new point is not closer to the battery location,
                 # reset the step
@@ -120,14 +114,7 @@ class Cables():
                 new_distance = self.compute_distance(x_battery, y_battery, x_loc, y_loc)
                 # if the new point is closer to the battery location, take the step
                 if new_distance < distance:
-                    distance = new_distance
-                    self.steps_count += 1
-
-                    # save the individual steps in the grid list in the dictionary of the house
-                    house_dict['grid'].append(f'{x_loc}, {y_loc}')
-
-                    self.x_list.append(x_loc)
-                    self.y_list.append(y_loc)
+                    distance = self.save_step(distance, new_distance, house_dict, x_loc, y_loc)
 
                 # if the new point is not closer to the battery location,
                 # reset the step
@@ -141,10 +128,9 @@ class Cables():
         by taking a random step, evaluating if this step is closer to the battery
         and repeating the process
         '''
-        self.steps_count = 0
 
         for battery in list_with_batteries:
-            color = 
+            # color =
             for house_dict in battery.dict['connected houses']:
 
                 # find x and y coordinates for the battery and connected house
@@ -162,5 +148,4 @@ class Cables():
 
                 self.try_steps(battery.x, battery.y, x_loc, y_loc, distance, house_dict)
 
-                plt.plot(self.x_list, self.y_list, color=color, linestyle='dashed')
-        return self.steps_count
+                plt.plot(self.x_list, self.y_list, 'k--')
