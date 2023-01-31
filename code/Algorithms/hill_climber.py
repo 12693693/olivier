9500de6f295d13b2f4d3b371d74d85c2aeb5e9bc
Frederique <frederique.mulder@gmail.com>
2023-01-31 15:07:35 +0100

    comments toegevoed

--- code/Algorithms/hill_climber.py
+++ code/Algorithms/hill_climber.py
@@ -22,82 +22,87 @@ class Hill_Climber():
 
 
     def fill_new_grid(self, house_dict, battery, function):
+        """
+        This function returns the function that is used for the laying the cables.
+        """
 
+        # make a dictionary of every possible algoritm that can be used
         function_dict = {'cable_90_degree.make_90_degrees_cables(houses, batteries)': 'cable_90_degree.make_90_degrees_cable(house_dict, battery)', 'cable_random.random_try(houses, batteries)': 'cable_random.random_try(house_dict, battery)', 'search_cables.run_search(houses, batteries)' : 'search_cables.search_cables(house_dict, battery)', 'further_cables.run_further(houses, batteries)' : 'cable_further.further_cables(house_dict, battery)' }
-        #print('function', function_dict[function])
-        # fill the grid of the newly added houses
 
-        #print(house_dict, battery)
-
-        #print(eval(function_dict[function]))
-        #print(cable_90_degree.make_90_degrees_cable(house_dict, battery))
         return eval(function_dict[function])
 
     def check_capacity(self, battery_1, battery_2, house_1, house_2):
-        if battery_1.capacity + house_1['output'] - house_2['output'] >= 0 and battery_2.capacity + house_2['output'] - house_1['output'] >= 0:
+        """
+        This function returns if it is possible to change the houses without
+        exceeding the battery capacity.
+        """
 
+        if battery_1.capacity + house_1['output'] - house_2['output'] >= 0 and battery_2.capacity + house_2['output'] - house_1['output'] >= 0:
             return True
 
         else:
             return False
 
     def choose_battery_and_houses(self, list_with_batteries):
+        """
+        This function chooses two houses that are going to be switched
+        """
 
         # choose two random batteries
         self.battery_1 = random.choice(list_with_batteries)
         self.battery_2 = random.choice(list_with_batteries)
 
+        # choose another battery if they are the same
         while self.battery_1 == self.battery_2:
             self.battery_2 = random.choice(list_with_batteries)
 
-        # choose two houses that were assigned to the batteries and remove them
-        # and the steps count from the costs
+        # choose two houses that were assigned to the batteries
         self.house_1 = random.choice(self.battery_1.dict['houses'])
         self.house_2 = random.choice(self.battery_2.dict['houses'])
 
 
     def switch_two_houses(self, new_smartgrid, function):
+        """
+        This function switches two houses and fills the lays the new cables.
+        """
+
         list_with_batteries = new_smartgrid.battery_list
 
+        # choose houses that are going to be switched
         self.choose_battery_and_houses(list_with_batteries)
 
-        #print(house_1)
-        #print(self.check_capacity(self.battery_1, self.battery_2, self.house_1, self.house_2))
+        # choose other houses if switching them exceeds the battery capacity
         while self.check_capacity(self.battery_1, self.battery_2, self.house_1, self.house_2) == False:
-            #print(self.check_capacity(self.battery_1, self.battery_2, self.house_1, self.house_2))
             self.choose_battery_and_houses(list_with_batteries)
 
         else:
-            #print(self.check_capacity(self.battery_1, self.battery_2, self.house_1, self.house_2))
+
+            # remove the houses from the batteries
             self.battery_1.dict['houses'].remove(self.house_1)
             self.battery_2.dict['houses'].remove(self.house_2)
 
 
-            #smartgrid.combined_list[0]['costs shared'] - (len(house_1['grid']) - 1) - (len(house_2['grid']) - 1)
-
-            # reset the grid
+            # reset the cables
             self.house_1['cables'] = []
             self.house_2['cables'] = []
 
-            # fill the grid of the houses
-            # string_function_1 = f'{self.fill_new_grid(function)}({self.house_1}, {self.battery_2})'
-            # print('string', string_function_1)
-            # self.steps_house_1 = eval(string_function_1)
-            # string_function_2 = self.fill_new_grid(self.house_2, self.battery_1, function)
-            # self.steps_house_2 = eval(string_function_2)
-
-            self.steps_house_1 = self.fill_new_grid(self.house_1, self.battery_2, function)
-            self.steps_house_2 = self.fill_new_grid(self.house_2, self.battery_1, function)
-
-            # switch the houses and add them to a new battery
+            # add the houses to the other battery
             self.battery_1.dict['houses'].append(self.house_2)
             self.battery_2.dict['houses'].append(self.house_1)
 
-    def check_solution(self, new_smartgrid):
-        #print('oud zonder', self.new_costs)
-        self.new_costs = new_smartgrid.get_costs(self.shared)
-        #print('nieuw', self.new_costs)
+            # make new paths of cables using the cable algorithm that you choose
+            self.steps_house_1 = self.fill_new_grid(self.house_1, self.battery_2, function)
+            self.steps_house_2 = self.fill_new_grid(self.house_2, self.battery_1, function)
 
+
+
+    def check_solution(self, new_smartgrid):
+        """
+        Checks and accepts better solutions than the current solution.
+        """
+        self.new_costs = new_smartgrid.get_costs(self.shared)
+
+        # accepts if the solution costs less
         if self.new_costs <= self.costs:
             self.smartgrid = new_smartgrid
             self.costs = self.new_costs
@@ -109,44 +114,14 @@ class Hill_Climber():
         self.iterations = iterations
 
         for iteration in range(iterations):
-            # Nice trick to only print if variable is set to True
             print(f'Iteration {iteration}/{iterations}, current value: {self.costs}')
 
-            # Create a copy of the graph to simulate the change
+            # create a copy of the graph to simulate the change
             new_smartgrid = copy.deepcopy(self.smartgrid)
 
             self.switch_two_houses(new_smartgrid, function)
-            #print('na switchen')
 
-            # Accept it if it is better
+            # accept it if it is better
             self.check_solution(new_smartgrid)
 
         return self.smartgrid
-
-        #my_smartgrid.make_output(args.district, shared_input)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # switch the assigned batteries of the houses
-
-    #Kies een random start state
-
-
-    #Herhaal:
-    #Doe een kleine random aanpassing
-#Als de state is verslechterd:
-#Maak de aanpassing ongedaan
--- code/Algorithms/simulated_annealing.py
+++ code/Algorithms/simulated_annealing.py
@@ -6,28 +6,22 @@ from .hill_climber import Hill_Climber
 class Simulated_Annealing(Hill_Climber):
 
     def __init__(self, smartgrid_solution, shared, temperature=1):
-        # Use the init of the Hillclimber class
+        # use the init of the Hillclimber class
         super().__init__(smartgrid_solution, shared)
 
-        # Starting temperature and current temperature
+        # starting temperature and current temperature
         self.T0 = temperature
         self.T = temperature
 
 
     def update_temperature(self):
         """
-        This function implements a *linear* cooling scheme.
+        This function implements a linear cooling scheme.
         Temperature will become zero after all iterations passed to the run()
         method have passed.
         """
 
         self.T = self.T - (self.T0 / self.iterations)
-
-        # Exponential would look like this:
-        # alpha = 0.99
-        # self.T = self.T * alpha
-
-        # where alpha can be any value below 1 but above 0
 
     def check_solution(self, new_smartgrid):
         """
@@ -36,19 +30,16 @@ class Simulated_Annealing(Hill_Climber):
         temperature.
         """
         self.new_costs = new_smartgrid.get_costs(self.shared)
-        #old_value = self.costs
 
-        # Calculate the probability of accepting this new graph
+        # calculate the probability of accepting this new smartgrid
         delta = self.new_costs - self.costs
         probability = math.exp(-delta / self.T)
 
-        # NOTE: Keep in mind that if we want to maximize the value, we use:
-        # delta = old_value - new_value
 
-        # Pull a random number between 0 and 1 and see if we accept the graph!
+        # pull a random number between 0 and 1 and see if we accept the graph!
         if random.random() < probability:
             self.smartgrid = new_smartgrid
             self.costs = self.new_costs
 
-        # Update the temperature
+        # update the temperature
         self.update_temperature()
